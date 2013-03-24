697ce12f1641e0c744ea1559d634d7041f8cb1f5
Niels Søholm <nmar@itu.dk>
2013-03-24 16:52:50 +0100

    Added a NetBeans project for the Okular example. Also extended and refactored the example a bit.

--- DBus Examples/DBusOkular/src/dbusokular/DBusOkular.java
+++ DBus Examples/DBusOkular/src/dbusokular/DBusOkular.java
@@ -1,8 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package dbus;
+package dbusokular;
 
 import java.text.ParseException;
 import org.freedesktop.dbus.BusAddress;
@@ -11,21 +7,24 @@ import org.freedesktop.dbus.Message;
 import org.freedesktop.dbus.MethodCall;
 import org.freedesktop.dbus.MethodReturn;
 import org.freedesktop.dbus.Transport;
+import org.freedesktop.dbus.UInt32;
 
 /**
  *
  * @author jan
  */
-public class DBUS {
+public class DBusOkular {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception  {
-        testAndExpose();
+        //testAndExpose();
+        talkToOkular();
     }
     
     public static void talkToOkular() throws Exception {
+        // Initialize D-Bus connection
         BusAddress address = new BusAddress(
             System.getenv("DBUS_SESSION_BUS_ADDRESS"));
         Transport conn = new Transport(address);
@@ -36,16 +35,72 @@ public class DBUS {
         MethodReturn response = (MethodReturn) conn.min.readMessage();
         System.out.println("My unique name is: "+response.getParameters()[0]);
        
-       
-        m = new MethodCall("org.kde.okular-3540", "/okular",
-        "org.kde.okular", "openDocument", (byte) 0, "s", "~/Downloads/dbus-java.pdf");
+        // Talk to Okular
+        String okularInstance = "org.kde.okular-7526";
+        
+        // Open document in Okular
+        m = new MethodCall(okularInstance, "/okular",
+        "org.kde.okular", "openDocument", (byte) 0, "s", "~/Downloads/jolie_wshandbook2012.pdf");
         conn.mout.writeMessage(m);
+        
+        // Go to next page (No parameters, one-way)
+        m = new MethodCall(
+            okularInstance, 
+            "/okular",
+            "org.kde.okular", 
+            "slotNextPage", (byte) 0, null);
+        conn.mout.writeMessage(m);
+        
+        // Go to specific page (Single uint parameter, one-way)
+        m = new MethodCall(
+            okularInstance, 
+            "/okular",
+            "org.kde.okular", 
+            "goToPage", (byte) 0, "u", 10);
+        conn.mout.writeMessage(m);
+        
+        // Get number of pages (No parameters, uint32 return value)
+        UInt32 numPages = NumberOfPages(conn, okularInstance);
+        System.out.println("Number of pages: " + numPages); 
+        
+    }
+    
+    public static UInt32 NumberOfPages(Transport conn, String okularIntance) throws Exception
+    {
+        UInt32 result = new UInt32(0L);
+        
+        // Make request
+        Message m = new MethodCall(
+                okularIntance, 
+                "/okular",
+                "org.kde.okular", 
+                "pages", (byte) 0, null);
+        long callId = m.getSerial();
+        conn.mout.writeMessage(m);
+        
+        // Read response
+        while(true)
+        {
+            m = conn.min.readMessage();
+            if(m != null && (m instanceof MethodReturn))
+            {
+                Message resp = (MethodReturn)m;
+                Long respId = new Long(resp.getReplySerial());
+                if(respId.equals(callId))
+                {
+                    result = (UInt32)resp.getParameters()[0];
+                    break;
+                }
+            }
+        }
+        return result;
     }
     
     public static void testAndExpose() throws Exception {
         BusAddress address = new BusAddress(
             System.getenv("DBUS_SESSION_BUS_ADDRESS"));
         
+        // Say hello to D-Bus
         //Transport conn = new Transport(address, true);
         Transport conn = new Transport(address);
         Message m = new MethodCall("org.freedesktop.DBus", "/",
@@ -53,12 +108,16 @@ public class DBUS {
         conn.mout.writeMessage(m);
         m = conn.min.readMessage();
         System.out.println("Response to Hello is: "+m);
+        
+        // Request a name
         m = new MethodCall("org.freedesktop.DBus", "/",
         "org.freedesktop.DBus", "RequestName", (byte) 0,
         "su", "org.testname", 0);
         conn.mout.writeMessage(m);
         m = conn.min.readMessage();
         System.out.println("Response to Requestname is: "+m);
+        
+        // Introsepctable "Hello service"
         while (true) { 
             m = conn.min.readMessage();
             System.out.println("Some message: "+m);
